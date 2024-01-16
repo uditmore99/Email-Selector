75b267a58b9658ec5fe4c6b5659036785e566f3b
uditmore99 <udit.more99@gmail.com>
2024-01-17 01:41:48 +0530

    Solved a bug where the dropdown was not appearing

--- components/EmailSelector.jsx
+++ components/EmailSelector.jsx
@@ -7,7 +7,6 @@ const EmailSelector = () => {
   const [selectedEmails, setSelectedEmails] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [showDropdown, setShowDropdown] = useState(false);
-  const [clickedTextBox, setClickedTextBox] = useState(false);
   const [darkMode, setDarkMode] = useState(true);
   const inputRef = useRef(null);
 
@@ -42,7 +41,7 @@ const EmailSelector = () => {
       // Deselect the last selected email on backspace
       const lastSelected = selectedEmails[selectedEmails.length - 1];
       setSelectedEmails(selectedEmails.slice(0, -1));
-      //   setSearchTerm(`${lastSelected.name} <${lastSelected.email}>`);
+      // setSearchTerm(`${lastSelected.name} <${lastSelected.email}>`);
       setSearchTerm("");
     }
   };
@@ -52,10 +51,19 @@ const EmailSelector = () => {
     document.body.classList.toggle("dark-mode", !darkMode);
   };
 
+  // const handleBlur = () => {
+  //   if (searchTerm != "") {
+  //     setShowDropdown(true);
+  //   } else setShowDropdown(false);
+  // };
+
   const handleBlur = () => {
-    setTimeout(() => {
-      setShowDropdown(false);
-    }, 150);
+    if (searchTerm != "") {
+      setShowDropdown(true);
+    } else
+      setTimeout(() => {
+        setShowDropdown(false);
+      }, 150);
   };
 
   return (
@@ -107,32 +115,30 @@ const EmailSelector = () => {
               type="text"
               placeholder="Type to add recipients"
               className="border text-black"
-              onClick={() => setShowDropdown(true) && setClickedTextBox(true)}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onFocus={() => setShowDropdown(true)}
+              onChange={(e) => setSearchTerm(e.target.value) && handleBlur}
               onKeyDown={handleKeyDown}
               value={searchTerm}
               ref={inputRef}
               onBlur={handleBlur}
             />
-            {showDropdown && (
+            {showDropdown && filteredEmails.length > 0 && (
               <div
                 className={`fixed  ${
                   darkMode ? "bg-gray-900 text-white " : "bg-white text-black"
                 } mt-12 w-64 rounded-md shadow-lg ring-1 ring-black ring-opacity-5`}
               >
-                {filteredEmails.length > 4 && (
-                  <div className="overflow-y-scroll w-fit max-h-48">
-                    {filteredEmails.map((email) => (
-                      <div
-                        key={email.email}
-                        className="p-2 cursor-pointer hover:bg-gray-600"
-                        onClick={() => handleSelectEmail(email)}
-                      >
-                        {email.name} - {email.email}
-                      </div>
-                    ))}
-                  </div>
-                )}
+                <div className="overflow-y-scroll w-fit max-h-48">
+                  {filteredEmails.map((email) => (
+                    <div
+                      key={email.email}
+                      className="p-2 cursor-pointer hover:bg-gray-600"
+                      onClick={() => handleSelectEmail(email)}
+                    >
+                      {email.name} - {email.email}
+                    </div>
+                  ))}
+                </div>
               </div>
             )}
           </div>

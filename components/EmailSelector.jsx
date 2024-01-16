3e7518eddbb144795eb159dd56b5718d2623c68d
uditmore99 <udit.more99@gmail.com>
2024-01-17 00:46:09 +0530

    Final commit

--- components/EmailSelector.jsx
+++ components/EmailSelector.jsx
@@ -58,81 +58,6 @@ const EmailSelector = () => {
     }, 150);
   };
 
-  //   return (
-  //     <>
-  //       <button
-  //         onClick={togDarkMode}
-  //         className={`absolute top-0 right-0 m-4 p-2 ${
-  //           darkMode ? "bg-gray-700 text-white" : "bg-white text-gray-800"
-  //         } rounded`}
-  //       >
-  //         {darkMode ? "Light Mode" : "Dark Mode"}
-  //       </button>
-  //       <div
-  //         className={`flex items-center justify-center h-screen ${
-  //           darkMode ? "bg-gray-800" : "bg-gray-100"
-  //         }`}
-  //       >
-  //         <h1
-  //           className={`text-3xl font-bold mb-4 ${
-  //             darkMode ? "text-white" : "text-gray-800"
-  //           }`}
-  //         >
-  //           Email Setter
-  //         </h1>
-  //         <div className="flex flex-wrap">
-  //           {selectedEmails.map((email) => (
-  //             <div
-  //               key={email.email}
-  //               className="bg-blue-500 text-white px-2 py-1 m-1 rounded-full"
-  //             >
-  //               {email.name}{" "}
-  //               <span
-  //                 className="cursor-pointer"
-  //                 onClick={() => handleRemoveEmail(email)}
-  //               >
-  //                 &#10006;
-  //               </span>
-  //             </div>
-  //           ))}
-  //           <input
-  //             type="text"
-  //             placeholder="Type to add recipients"
-  //             className="border p-2"
-  //             onClick={() => setShowDropdown(true) && setClickedTextBox(true)}
-  //             onChange={(e) => setSearchTerm(e.target.value)}
-  //             onKeyDown={handleKeyDown}
-  //             value={searchTerm}
-  //             ref={inputRef}
-  //             onBlur={handleBlur}
-  //           />
-  //         </div>
-  //         {showDropdown && (
-  //           <div
-  //             className={`sticky ${
-  //               darkMode ? "bg-gray-400" : "bg-white"
-  //             } mt-2 w-64 rounded-md shadow-lg ring-1 ring-black ring-opacity-5`}
-  //           >
-  //             {filteredEmails.length > 4 && (
-  //               <div className="overflow-y-auto max-h-32">
-  //                 {filteredEmails.map((email) => (
-  //                   <div
-  //                     key={email.email}
-  //                     className="p-2 cursor-pointer hover:bg-gray-200"
-  //                     onClick={() => handleSelectEmail(email)}
-  //                   >
-  //                     {email.name}
-  //                   </div>
-  //                 ))}
-  //               </div>
-  //             )}
-  //           </div>
-  //         )}
-  //       </div>
-  //     </>
-  //   );
-  // };
-
   return (
     <>
       <button
@@ -155,11 +80,18 @@ const EmailSelector = () => {
         >
           Email Setter
         </h1>
-        <div className="flex flex-col items-center">
+
+        <div
+          className={`flex flex-row items-center rounded-3xl ${
+            darkMode ? "bg-gray-900" : "bg-white"
+          }`}
+        >
           {selectedEmails.map((email) => (
             <div
               key={email.email}
-              className="bg-blue-500 text-white px-2 py-1 m-1 rounded-full"
+              className={` p-1 rounded-full ${
+                darkMode ? "text-white " : "text-black"
+              }`}
             >
               {email.name}{" "}
               <span
@@ -170,38 +102,40 @@ const EmailSelector = () => {
               </span>
             </div>
           ))}
-          <input
-            type="text"
-            placeholder="Type to add recipients"
-            className="border p-2"
-            onClick={() => setShowDropdown(true) && setClickedTextBox(true)}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyDown={handleKeyDown}
-            value={searchTerm}
-            ref={inputRef}
-            onBlur={handleBlur}
-          />
-          {showDropdown && (
-            <div
-              className={`sticky ${
-                darkMode ? "bg-gray-400" : "bg-white"
-              } mt-2 w-64 rounded-md shadow-lg ring-1 ring-black ring-opacity-5`}
-            >
-              {filteredEmails.length > 4 && (
-                <div className="overflow-y-auto max-h-32">
-                  {filteredEmails.map((email) => (
-                    <div
-                      key={email.email}
-                      className="p-2 cursor-pointer hover:bg-gray-200"
-                      onClick={() => handleSelectEmail(email)}
-                    >
-                      {email.name}
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          )}
+          <div className="flex flex-col items-center relative">
+            <input
+              type="text"
+              placeholder="Type to add recipients"
+              className="border text-black"
+              onClick={() => setShowDropdown(true) && setClickedTextBox(true)}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              onKeyDown={handleKeyDown}
+              value={searchTerm}
+              ref={inputRef}
+              onBlur={handleBlur}
+            />
+            {showDropdown && (
+              <div
+                className={`fixed ${
+                  darkMode ? "bg-gray-900 " : "bg-white "
+                } mt-12 w-64 rounded-md shadow-lg ring-1 ring-black ring-opacity-5`}
+              >
+                {filteredEmails.length > 4 && (
+                  <div className="overflow-y-scroll w-fit max-h-48">
+                    {filteredEmails.map((email) => (
+                      <div
+                        key={email.email}
+                        className="p-2 cursor-pointer hover:bg-gray-600"
+                        onClick={() => handleSelectEmail(email)}
+                      >
+                        {email.name} - {email.email}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
